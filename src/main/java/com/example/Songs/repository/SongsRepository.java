df92bc32a8fd03647be203b9ae60e23fb1adfc13
Alamgeer Thanwal <alamthanwal@gmil.com>
2025-10-17 22:15:09 +0530

    Initial commit

--- src/main/java/com/example/Songs/repository/SongsRepository.java
+++ src/main/java/com/example/Songs/repository/SongsRepository.java
@@ -2,9 +2,10 @@ package com.example.Songs.repository;
 
 import com.example.Songs.model.Songs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 public interface SongsRepository extends JpaRepository<Songs, Long> {
     // Get all songs where type = "Trending Songs"
     List<Songs> findByType(String type);
